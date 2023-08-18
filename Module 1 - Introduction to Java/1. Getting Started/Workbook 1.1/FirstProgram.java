dba51ce0319c3abca7661eb93c96dfbe84e8c755
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-08-18 21:55:19 +0600

    Initialized

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -6,6 +6,7 @@ public class FirstProgram{
         System.out.println("Java > Python.");
         System.out.println("I spilled Java all over my paper.");
         System.out.println("My dog ate my Java.");
+        System.out.println("ok");
 
 
     }
